327bf7167101af21d1466afa59811ae98cf4649a
Kaan Arslan <115875320+kaan-ders@users.noreply.github.com>
2023-01-29 13:56:41 +0300

    initial commit

---
+++ Controllers/MusteriController.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MvcApp.Models;
+
+namespace MvcApp.Controllers
+{
+    public class MusteriController : Controller
+    {
+        private List<Musteri> Musteriler = new List<Musteri>
+        {
+            new Musteri
+            {
+                Id = 1,
+                Adi = "Ahmet",
+                Ulke = "Almanya",
+                SiparisSayisi = 10
+            },
+            new Musteri
+            {
+                Id = 2,
+                Adi = "Ayşe",
+                Ulke = "Amerika",
+                SiparisSayisi = 5
+            }
+        };
+
+        //action - sayfa
+        public IActionResult Index()
+        {
+            return View(Musteriler);
+        }
+
+        //action - sayfa
+        public IActionResult Detay(int id)
+        {
+            Musteri musteri = null;
+
+            foreach (var item in Musteriler)
+            {
+                if (item.Id == id)
+                {
+                    musteri = item;
+                    break;
+                }
+            }
+
+            return View(musteri);
+        }
+    }
+}
---
+++ Models/Musteri.cs
@@ -0,0 +1,10 @@
+﻿namespace MvcApp.Models
+{
+    public class Musteri
+    {
+        public int Id { get; set; }
+        public string Adi { get; set; }
+        public string Ulke { get; set; }
+        public int SiparisSayisi { get; set; }
+    }
+}
